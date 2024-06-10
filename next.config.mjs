4b12dd78eebbeccf573614b487b6266f2398a9e5
LeeSeungHun <dos07008@naver.com>
2024-06-11 00:27:16 +0900

    feat: react 19 컴파일러 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    reactCompiler: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
